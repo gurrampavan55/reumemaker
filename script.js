33467d7a5100e6e2d6fb0e0f0133da8d25b512ae
gurrampavan55 <132094389+gurrampavan55@users.noreply.github.com>
2025-05-22 22:07:13 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,14 @@
+function generateResume() {
+  // Copy input values to output fields
+  document.getElementById('outName').innerText = document.getElementById('name').value;
+  document.getElementById('outEmail').innerText = document.getElementById('email').value;
+  document.getElementById('outPhone').innerText = document.getElementById('phone').value;
+
+  // Show only resume output
+  document.getElementById('resumeOutput').style.display = 'block';
+}
+
+function downloadPDF() {
+  const element = document.getElementById('resumeOutput');
+  html2pdf().from(element).save();
+}
